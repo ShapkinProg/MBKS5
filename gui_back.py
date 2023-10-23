979d1df9a506a00ad30935a9a088192617b97681
ShapkinProg <shlya12332q1@yandex.ru>
2023-10-23 20:24:27 +0300

    Add new function for gui backend

--- gui_back.py
+++ gui_back.py
@@ -1,7 +1,3 @@
-# Author: David Colonel
-# Date: April 30, 2023
-# Description: Backend functions required for GUI (gui.py)
-
 import back_functions as back_funcs
 
 permissions_masks = {
@@ -54,3 +50,57 @@ def get_integrity_level(pid):
 def set_integrity_level(pid, new_level):
     res = back_funcs.ChangeIntegrity(int(pid), new_level)
     return res
+
+# returns acl for file
+def get_acl(filepath):
+    acl = back_funcs.GetDACL(filepath)
+    res = {}
+    for ace in acl:
+        split_res = ace.split(": ")
+        obj_perm = {}
+        keys = permissions_masks.keys()
+        for key in keys:
+            if int(split_res[1]) & permissions_masks[key] == permissions_masks[key]:
+                obj_perm[key] = 1
+            else:
+                obj_perm[key] = 0
+
+        # get deny or allow status, add to name of user and delete from permissions list
+        if (obj_perm['Allow/Deny'] == 0):
+            allow_or_deny = "(deny)"
+        else:
+            allow_or_deny = "(allow)"
+        obj_perm.pop('Allow/Deny')
+
+        res[split_res[0]+":"+allow_or_deny] = obj_perm
+    return res
+
+
+# changes subject acl
+def change_subject_acl(filepath, new_subject_ace, old_permissions):
+    # get parameters for ChangeDACL()
+    subj_name = new_subject_ace['id'].split(":")[0]
+    subj_name = subj_name.split(" (")[0]
+    allow_or_deny = new_subject_ace['id'].split(":")[1]
+    if allow_or_deny == '(deny)':
+        is_deny = True
+    else:
+        is_deny = False
+
+    # for perm in old_permissions.keys():
+    #     # if permission is changed
+    #     if(old_permissions[perm] != new_subject_ace['permissions'][perm]):
+    #         perm_mask = permissions_masks[perm]
+    #         if new_subject_ace['permissions'][perm] == 0:
+    #             is_delete = True
+    #         else:
+    #             is_delete = False
+    #         back_funcs.ChangeDACL(filepath, subj_name, perm_mask, is_delete, is_deny)
+
+    perm_mask = 0
+    for perm in new_subject_ace['permissions'].keys():
+        if new_subject_ace['permissions'][perm] == 1:
+            perm_mask |= permissions_masks[perm]
+
+    if (is_deny == False): perm_mask |= 0x100000
+    back_funcs.ChangeDACL(filepath, subj_name, perm_mask)
